00a6137d9033be17ef208b46e464773376694160
ukAnandhu <anandhuuthaman6@gmail.com>
2021-10-18 12:42:26 +0530

    initial commit

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { AiTwotoneDelete } from "react-icons/ai";
 
 function TodoList({text, todo, todos, setTodo}) {
     const deleteHandler = () => {  
@@ -9,7 +10,7 @@ function TodoList({text, todo, todos, setTodo}) {
         <div className="list">
             <div className="list-items">
                 <li className="list__values">{text}</li> 
-                <button className="list__button" onClick={deleteHandler}>delete</button>
+                <div className="list__button" onClick={deleteHandler}><AiTwotoneDelete size='30px'/></div>
             </div>
         </div>
     )
